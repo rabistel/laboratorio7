5aee1c2be1442d5e9ed7668b5f4f4d110af8858a
julianamette <julianamette3@gmail.com>
2020-03-19 17:24:41 -0300

    Add files via upload
    Estos son los que tenemos que usar

---
+++ arq_training.py
@@ -0,0 +1,87 @@
+##
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+#Primero cargo los datos que voy a usar para las pruebas de efectividad
+
+trn_delay = np.load('delays_medidos.npy')
+trn_tgt = np.load('fuentes_pos.npy')
+
+trn_data = np.zeros( (len(trn_delay), len(trn_delay[0]) + 3) ) 
+trn_data[:, :len(trn_delay[0])] = trn_delay
+trn_data[:, len(trn_delay[0]):] = trn_tgt
+
+#trn_data son los datos de entrenamiento 
+print('Tamaño del dataset:', len(trn_delay))
+####
+
+#seteo los parametros del dataset
+B = 32 #batch_size
+P = len(trn_data) #Longitud de mi dataset
+N = len(trn_delay[0]) #Cantidad de unidades de entrada
+N = 20
+C = 3 #Cantidad de unidades de salida son 3 porque es la salida x, y, z
+#####
+#Uso el data loader para tener todo bien puesto 
+trn_load = torch.utils.data.DataLoader(dataset = trn_data, batch_size = B, shuffle = True)
+
+#Declaro la clase de mlp
+class MLP(torch.nn.Module):
+    def __init__(_,sizes):
+        super().__init__()
+        _.layers = torch.nn.ModuleList()
+        for i in range(len(sizes)-1):
+            _.layers.append(torch.nn.Linear(sizes[i],sizes[i+1]))
+    def forward(_,x):
+        h = x
+        for hidden in _.layers[:-1]: #Todas las capas ocultas que no son la ultima, les pongo una sigmoid
+            h = torch.tanh(hidden(h))
+        output = _.layers[-1] #A la ultima capa le pongo una softmax
+        y = output(h)
+        #y = output(h)
+        return y
+#########
+
+#arqs = [ [256], [512], [64, 32], [64, 64], [128, 64], [128, 128], [128, 64, 32] , [256, 128], [32, 32, 32], [64, 64, 64], [32,32,32,32], [64,64,64,64], [128,64,32,16]]
+arqs = [[64,64,64,64], [0]] 
+i = 0
+
+for arq in arqs:
+	arq.insert(0,N)
+	arq.append(C)
+	model = MLP(arq)
+	costf = torch.nn.MSELoss()
+	optim = torch.optim.RMSprop(model.parameters(),lr = 1e-4)
+
+	E, t = 1, 0
+	errs = []
+
+	model.train() #Le aviso al programa que voy a empezar a entrenar
+
+	while  E > 1e-5 and t < 1500: #Dos condiciones para poder salir 
+	    e = []
+	    for row in trn_load: #Esto es un minibatch
+	        optim.zero_grad() #Reseteo los gradientes
+	        x =  row[:, :len(trn_delay[0])].float() #El -1 significa que no le especifico ese tamano, solo me importa el N. #Osea calculo la cantidad de filas a mano para que este todo bien
+	        #x = x[:,[0,1,2,3,9,10,11,17,18,24,35,36,37,38,39,40,41,42,43,44]]
+	        z =  row[:, len(trn_delay[0]):].float() #Tantas filas como labels me vinieron en mi dataset y tantas columnas como clases tengo
+	        y = model(x) #Calculo el modelo para mis inputs x
+	        error = costf(y,z) #Calculo el error
+	        error.backward() #'Propago' los errores
+	        optim.step() #Aplico el gradiente
+	        e.append(error.item()) #Estos son los errores dentro del batch
+	        
+	    t += 1 #Recien cuando termino todos los batch, termine una epoca
+	    E = sum(e)/len(e) #El error en cada epoca es el promedio del error que obtuve en cada batch, en esa epoca
+	    errs.append(E)
+	    if t%10 == 0:
+	        print(t) #Cada 10 epocas, printeo el numero de epoca
+	        print('error', E)
+
+	model.eval() #Le aviso al programa que apartir de ahora voy a emepzar a evaluar el modelo
+
+	nombre = str(i)
+	np.save(nombre + '.npy' , errs)
+	torch.save(model.state_dict(), nombre)
+	i += 1
---
+++ model_testing.py
@@ -0,0 +1,133 @@
+#Cargo el modelo ya entrenado y lo testeo
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+###############################################################################
+#CREO EL MODELO PARA DESPUES PODER CARGAR MI MODELO YA ENTRENADO
+'''
+#Todo este bloque verde medio que esta al pedo
+#Cargo los datos que se usaron para el entrenamiento
+trn_delay = np.load('Datasets\ds1_delays_medidos.npy')
+trn_tgt = np.load('Datasets\ds1_fuentes_pos.npy')
+
+trn_data = np.zeros((len(trn_delay), len(trn_delay[0]) + 3)) 
+trn_data[:, :len(trn_delay[0])] = trn_delay
+trn_data[:, len(trn_delay[0]):] = trn_tgt
+
+#trn_data son los datos de entrenamiento 
+print('Tamaño del dataset:', len(trn_delay))
+
+#Seteo los parametros del dataset
+B = 32 #batch_size
+P = len(trn_data) #Longitud de mi dataset
+'''
+#Cantidad de unidades de entrada:
+N = 45#len(trn_delay[0])
+#N = 20
+#Si estoy usando los 45 delays, entonces N = len(trn_delay[0])
+#Si estoy usando solo los delays mas significativos, entonces N = 20
+C = 3 #Cantidad de unidades de salida son 3 porque es la salida x, y, z
+
+#Uso el data loader para tener todo bien puesto 
+#trn_load = torch.utils.data.DataLoader(dataset = trn_data, batch_size = B, shuffle = True)
+
+#Declaro la clase de mlp
+class MLP(torch.nn.Module):
+    def __init__(self,sizes):
+        super().__init__()
+        self.layers = torch.nn.ModuleList()
+        for i in range(len(sizes)-1):
+            self.layers.append(torch.nn.Linear(sizes[i],sizes[i+1]))
+    def forward(self,x):
+        h = x
+        for hidden in self.layers[:-1]:
+            h = torch.tanh(hidden(h))
+        output = self.layers[-1]
+        y = output(h)
+        return y
+
+arqs = [[256], [512], [64, 32], [64, 64], [128, 64], [128, 128], [128, 64, 32] , [256, 128], [32, 32, 32], [64, 64, 64], [32,32,32,32], [64,64,64,64], [128,64,32,16]]
+arq = arqs[11] ##############Elijo que arquitectura quiero
+
+arq.insert(0,N)
+arq.append(C)
+model = MLP(arq)
+
+###############################################################################
+
+###CARGO MI MODELO YA ENTRENADO
+model.load_state_dict(torch.load('11_1500epochs'))
+
+###TESTEO EL MODELO
+model.eval() #Empiezo a evaluar el modelo
+
+#Cargo los datos de testeo
+tst_delay = np.load('Datasets\muchos_delays_medidos.npy')
+tst_tgt = np.load('Datasets\muchas_fuentes_pos.npy')
+
+tst_data = np.zeros((len(tst_delay), len(tst_delay[0]) + 3)) 
+tst_data[:, :len(tst_delay[0])] = tst_delay
+tst_data[:, len(tst_delay[0]):] = tst_tgt
+
+#Uso el data loader para tener todo bien puesto 
+BB = 1 #Batch size del dataset de testeo
+tst_load = torch.utils.data.DataLoader(dataset = tst_data, batch_size = BB, shuffle = True)
+
+#Funcion de costo que uso
+costf = torch.nn.MSELoss()
+
+e = [] #Cada elemento de e va a ser el error de cada batch del tst_load
+counter = 0
+with torch.no_grad():
+    for row in tst_load: #Esto es un minibatch
+        counter += 1
+        if counter%10 == 0:
+            print(counter)
+        x = row[:, :len(tst_delay[0])].float()
+        z = row[:, len(tst_delay[0]):].float()
+        #x = x[:,[0,1,2,3,9,10,11,17,18,24,35,36,37,38,39,40,41,42,43,44]] #Esta linea tiene que estar sin comentar si quiero sacar las neuronas que son menos importantes #Si NO esta comentada, entonces N tiene que ser 20
+        y = model(x)
+        error = costf(y,z)
+        e.append(error.item())
+        
+    error_promedio = np.mean(e) #El error en cada epoca es el promedio del error que obtuve en cada batch, en esa epoca
+    desviacion_error = np.std(e) #Desviacion estandar de los errores
+    
+print('El error promedio de la arquitectura es ' + str(error_promedio))
+print('El desvio estandar de la arquitectura es ' + str(desviacion_error))
+
+print('#####')
+#El error real que queremos (distancia entre la fuente real y la obtenida) no es exactamente lo que me da la cost function MSELoss
+#Aca defino bien el error
+e_posta = np.zeros(len(e))
+for i in range(len(e)):
+    e_posta[i] = np.sqrt(e[i]*3)
+    
+#np.save('11_errores_testeo_m', e_posta)
+    
+e_posta_prom = np.mean(e_posta)
+e_posta_desv = np.std(e_posta)
+
+print('El error promedio posta es ' + str(e_posta_prom) + 'm')
+print('El desvio estandar posta es ' + str(e_posta_desv) + 'm')
+
+######################
+'''
+#Ploteo los errores de entrenamiento
+err  = np.load('10.npy')
+
+f1 = plt.figure()
+plt.plot(err,'.-')
+plt.title('Errores de entrenamiento')
+'''
+
+'''
+#Para ver los pesos de una capa:
+f = plt.plot()
+plt.imshow(np.abs(model.layers[0].weight.detach().numpy()), cmap = 'plasma')
+plt.colorbar(label = 'Módulo de las activaciones')
+plt.xlabel('Diferencias temporales')
+plt.ylabel('Neuronas de la primera capa oculta')
+'''
