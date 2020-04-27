a734b4dddbcb113752021c961a2547f7bb60b642
Julian Amette Estrada <julianamette3@gmail.com>
2020-04-27 19:27:36 -0300

    Chequeo de los tiempos

--- señales/importar_csv.py
+++ señales/importar_csv.py
@@ -4,17 +4,15 @@ import matplotlib.pyplot as plt
 
 
 nombreArchivo = 'TEST_20000000_000702.csv' #Aca va el nombre del archivo
-
 ti = time.time()
-
-#data = np.genfromtxt(nombreArchivo, skip_header = 2, dtype = 'U')
 data = np.loadtxt(nombreArchivo, skiprows = 2, dtype = 'U')
 print('Importado')
+t1 = time.time()
 overrunsChecking =  np.char.find(data,'Overruns')
 print('Overruns chequeados')
 overrunsPositions = np.where(overrunsChecking == 0)
+t2 = time.time()
 
-t1 = time.time()
 print(len(overrunsPositions))
 print('Tiempo parcial', t1-ti)
 counter = 0
@@ -26,17 +24,13 @@ for pos in overrunsPositions[0]:
 	counter += val
 	overruns += 1
 
-
 tf = time.time()
 
-print('tardo:', tf-ti)
+print('tardo en cargar el archivo: ', t1-ti)
+print('tardo en ver overruns: ', t2-t1)
+print('tardo en reemplazar: '  , tf-t2)
 
 
 
 
 
-t0 = time.time()
-data = np.loadtxt(nombreArchivo, skiprows = 2, dtype = 'U')
-t1 = time.time()
-data = np.genfromtxt(nombreArchivo, skip_header = 2, dtype = 'U')
-t2 = time.time()
