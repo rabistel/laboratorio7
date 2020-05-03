c53451e5db28006b0d2bd7faa109228cc648bebd
Julian Amette Estrada <julianamette3@gmail.com>
2020-05-03 20:30:21 -0300

    Programa mas prolijo y coherente

--- AI/model_testing.py
+++ AI/model_testing.py
@@ -4,34 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-###############################################################################
-#CREO EL MODELO PARA DESPUES PODER CARGAR MI MODELO YA ENTRENADO
-'''
-#Todo este bloque verde medio que esta al pedo
-#Cargo los datos que se usaron para el entrenamiento
-trn_delay = np.load('Datasets\ds1_delays_medidos.npy')
-trn_tgt = np.load('Datasets\ds1_fuentes_pos.npy')
+#Cargo los datos de testeo
+tst_delay = np.load('delays_medidos_test.npy')
+tst_tgt = np.load('fuentes_pos_test.npy')
 
-trn_data = np.zeros((len(trn_delay), len(trn_delay[0]) + 3)) 
-trn_data[:, :len(trn_delay[0])] = trn_delay
-trn_data[:, len(trn_delay[0]):] = trn_tgt
-
-#trn_data son los datos de entrenamiento 
-print('Tamaño del dataset:', len(trn_delay))
-
-#Seteo los parametros del dataset
-B = 32 #batch_size
-P = len(trn_data) #Longitud de mi dataset
-'''
-#Cantidad de unidades de entrada:
-N = 45#len(trn_delay[0])
-#N = 20
-#Si estoy usando los 45 delays, entonces N = len(trn_delay[0])
-#Si estoy usando solo los delays mas significativos, entonces N = 20
-C = 3 #Cantidad de unidades de salida son 3 porque es la salida x, y, z
+tst_data = np.zeros((len(tst_delay), len(tst_delay[0]) + 3)) 
+tst_data[:, :len(tst_delay[0])] = tst_delay
+tst_data[:, len(tst_delay[0]):] = tst_tgt
 
 #Uso el data loader para tener todo bien puesto 
-#trn_load = torch.utils.data.DataLoader(dataset = trn_data, batch_size = B, shuffle = True)
+BB = 1 #Batch size del dataset de testeo
+tst_load = torch.utils.data.DataLoader(dataset = tst_data, batch_size = BB, shuffle = True)
+
+N = len(tst_data[0])
+C = 3 #Cantidad de unidades de salida son 3 porque es la salida x, y, z
 
 #Declaro la clase de mlp
 class MLP(torch.nn.Module):
@@ -63,17 +49,6 @@ model.load_state_dict(torch.load('0'))
 ###TESTEO EL MODELO
 model.eval() #Empiezo a evaluar el modelo
 
-#Cargo los datos de testeo
-tst_delay = np.load('delays_medidos_test.npy')
-tst_tgt = np.load('fuentes_pos_test.npy')
-
-tst_data = np.zeros((len(tst_delay), len(tst_delay[0]) + 3)) 
-tst_data[:, :len(tst_delay[0])] = tst_delay
-tst_data[:, len(tst_delay[0]):] = tst_tgt
-
-#Uso el data loader para tener todo bien puesto 
-BB = 1 #Batch size del dataset de testeo
-tst_load = torch.utils.data.DataLoader(dataset = tst_data, batch_size = BB, shuffle = True)
 
 #Funcion de costo que uso
 costf = torch.nn.MSELoss()
@@ -87,7 +62,6 @@ with torch.no_grad():
             print(counter)
         x = row[:, :len(tst_delay[0])].float()
         z = row[:, len(tst_delay[0]):].float()
-        #x = x[:,[0,1,2,3,9,10,11,17,18,24,35,36,37,38,39,40,41,42,43,44]] #Esta linea tiene que estar sin comentar si quiero sacar las neuronas que son menos importantes #Si NO esta comentada, entonces N tiene que ser 20
         y = model(x)
         error = costf(y,z)
         e.append(error.item())
