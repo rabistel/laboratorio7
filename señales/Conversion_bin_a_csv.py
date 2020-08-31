e1b408e742a448272e543c1c7ebefe93ff1d5caa
Julian Amette Estrada <julianamette3@gmail.com>
2020-08-31 12:16:00 -0300

    Mejoras para uso

--- señales/Conversion_bin_a_csv.py
+++ señales/Conversion_bin_a_csv.py
@@ -1,28 +1,23 @@
-#Este codigo tiene el unico proposito de convertir un archivo .bin en uno .csv
-
+#
+# Este codigo tiene como fin tranformar archivos de bin a csv (y guardarlos)
+#
 from subprocess import call
 from os import listdir
 
-'''
-########################################################################
-#Esto es para hacer la convertir un solo archivo
+####### Parametros Editables ###############
+convertir_archivo = False
+convertir_carpeta = True
+nombre_archivo 	  = 'archivo'
+nombre_carpeta	  = 'nombre_carpeta'
+############################################
 
-pop = '2' #Nombre del archivo .bin (sin la extension) que quiero transformar a .csv
-call(['./read', pop+'.bin', pop+'.csv']) #Esta linea es la que crea el archivo .csv
+if convertir_archivo:
+	pop = 'nombre_archivo' 	
+	call(['./read', pop+'.bin', pop+'.csv']) 	
 
-#Listo, el archivo .csv se crea y se guarda
-########################################################################
-'''
+if convertir_carpeta:
+	for item in listdir(carpeta):
+	    call(['./read', carpeta+item[:-4]+'.bin', carpeta+item[:-4]+'.csv'])
 
-########################################################################
-#Esto es por si quiero hacer todo el proceso para todos los archivos de una misma carpeta sin tener que hacerlo para cada uno de los archivos por separado
-
-carpeta = 'Mas_mediciones_con_distintas_frecuencias/27/D/' #CUIDADO: Siempre el último caracter tiene que ser un /. Sino, no va a tirar error pero no va a hacer la conversión.
-
-for item in listdir(carpeta):
-    
-    call(['./read', carpeta+item[:-4]+'.bin', carpeta+item[:-4]+'.csv'])
-
-########################################################################
 
 
