c5f6b16280954ab3243adcd13b6aa524fa10cf86
Julian Amette Estrada <julianamette3@gmail.com>
2020-08-31 11:26:30 -0300

    Cambios arq training

--- AI/arq_training.py
+++ AI/arq_training.py
@@ -1,34 +1,35 @@
-##
+#
+# Este codigo tiene como fin entrenar la arquitectura del perceptron multicapa
+#
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-#Primero cargo los datos que voy a usar para las pruebas de efectividad
+#### Parametros Editables ###############
+B   = 250              #batch_size      
+arq = [64,64,64,64]    #arquitectura
+nro_epocas = 1500      #Nro de epocas    
+#########################################
 
-trn_delay = np.load('delays_medidos.npy')
-trn_tgt = np.load('fuentes_pos.npy')
+trn_delay   = np.load('delays_medidos.npy')  
+trn_tgt     = np.load('fuentes_pos.npy')     
 
-trn_data = np.zeros( (len(trn_delay), len(trn_delay[0]) + 3) ) 
+trn_data                        = np.zeros( (len(trn_delay), len(trn_delay[0]) + 3) ) 
 trn_data[:, :len(trn_delay[0])] = trn_delay
 trn_data[:, len(trn_delay[0]):] = trn_tgt
 
 device = torch.device( "cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-#trn_data son los datos de entrenamiento 
 print('Tamaño del dataset:', len(trn_delay))
-####
 
-#seteo los parametros del dataset
-B = 128 #batch_size
-P = len(trn_data) #Longitud de mi dataset
-N = len(trn_delay[0]) #Cantidad de unidades de entrada
-C = 3 #Cantidad de unidades de salida son 3 porque es la salida x, y, z
-#####
-#Uso el data loader para tener todo bien puesto 
+###
+P = len(trn_data)           
+N = len(trn_delay[0])       
+C = 3                       
+###
+
 trn_load = torch.utils.data.DataLoader(dataset = trn_data, batch_size = B, shuffle = True)
 
-#Declaro la clase de mlp
 class MLP(torch.nn.Module):
     def __init__(_,sizes):
         super().__init__()
@@ -37,50 +38,51 @@ class MLP(torch.nn.Module):
             _.layers.append(torch.nn.Linear(sizes[i],sizes[i+1]))
     def forward(_,x):
         h = x
-        for hidden in _.layers[:-1]: #Todas las capas ocultas que no son la ultima, les pongo una sigmoid
+        for hidden in _.layers[:-1]: 
             h = torch.tanh(hidden(h))
-        output = _.layers[-1] #A la ultima capa le pongo una softmax
+        output = _.layers[-1] 
         y = output(h)
-        #y = output(h)
         return y
-#########
-
-arq = [64,64,64,64]
 
 arq.insert(0,N)
 arq.append(C)
+
 model = MLP(arq).to(device)
 costf = torch.nn.MSELoss(reduce = False)
 optim = torch.optim.RMSprop(model.parameters(),lr = 1e-4)
 
-E, t = 1, 0
+t    = 0
 errs = []
 
-model.train() #Le aviso al programa que voy a empezar a entrenar
+model.train() 
 
-while  E > 1e-5 and t < 1500: #Dos condiciones para poder salir 
+while t < nro_epocas: 
     e = np.array([])
-    for row in trn_load: #Esto es un minibatch
-        optim.zero_grad() #Reseteo los gradientes
-        x =  row[:, :len(trn_delay[0])].float().to(device) #El -1 significa que no le especifico ese tamano, solo me importa el N. #Osea calculo la cantidad de filas a mano para que este todo bien
-        z =  row[:, len(trn_delay[0]):].float().to(device) #Tantas filas como labels me vinieron en mi dataset y tantas columnas como clases tengo
-        y = model(x) #Calculo el modelo para mis inputs x
+    for row in trn_load:
+        optim.zero_grad()
+        x =  row[:, :len(trn_delay[0])].float().to(device)
+        z =  row[:, len(trn_delay[0]):].float().to(device)
+        y = model(x)
         cost = costf(y,z)
-        error = cost.mean() #Calculo el error
-        error.backward() #'Propago' los errores
-        optim.step() #Aplico el gradiente
+        error = cost.mean()
+        error.backward()
+        optim.step()
         e = np.append(e, cost.sum(dim = 1).sqrt().mean().item())
         
-    t += 1 #Recien cuando termino todos los batch, termine una epoca
-    E = np.mean(e) #El error en cada epoca es el promedio del error que obtuve en cada batch, en esa epoca
+    t += 1 
+    E = np.mean(e) 
     errs.append(E)
+    
     if t%10 == 0:
-        print(t) #Cada 10 epocas, printeo el numero de epoca
-        print('error', E)
+        print(t)
+        print('Error', E)
+    
+    if t%25 == 0:
+        np.save('errores.npy' , errs)
+        torch.save(model.state_dict(), 'modelo_' + str(t) +'_epocas')
 
-model.eval() #Le aviso al programa que apartir de ahora voy a emepzar a evaluar el modelo
+model.eval() 
 
 
-np.save('errores.npy' , errs)
-torch.save(model.state_dict(), 'modelo_entrenado')
+
 
