d4781fb1b58b19f7553774b58dfcde2c89e4cb9c
Julian Amette Estrada <julianamette3@gmail.com>
2020-08-31 11:33:16 -0300

    Posibilidad de reentrenamiento

--- AI/arq_training.py
+++ AI/arq_training.py
@@ -9,7 +9,7 @@ import torch
 B   = 250              #batch_size      
 arq = [64,64,64,64]    #arquitectura
 nro_epocas = 1500      #Nro de epocas
-reentrenamiento = 0    # 0 --> No|1--> Si
+reentrenamiento = False    
 modelo_reentrenamiento = 'modelo'
 epoca0 = 0             #epocas preentrenadas
 ############################################
@@ -52,7 +52,7 @@ arq.append(C)
 
 model = MLP(arq).to(device)
 
-if reentrenamiento == '1':
+if reentrenamiento:
     model.load_state_dict(torch.load(modelo_reentrenamiento))
 
 costf = torch.nn.MSELoss(reduce = False)
